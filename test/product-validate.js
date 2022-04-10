59eb1a60432c2b0884fd884b756b68afea7e99ed
spti <tobetodee@gmail.com>
2022-04-10 13:48:27 +0300

    product, validation, test filterErrors: cleanup

--- test/product-validate.js
+++ test/product-validate.js
@@ -2,43 +2,38 @@ import {toTree} from 'ajv-errors-to-data-tree'
 import {default, filterErrors, _validate} from '../src/_product-validate.js'
 
 function filterErrors() {
-    describe("missing isInSale, no fields", () => {
+    describe("no fields and: 1. missing isInSale; 2. invalid isInSale. Contains a single error:", () => {
         _validate({})
-        const tree = toTree(_validate.errors)
+        const missing = toTree(_validate.errors)
 
-        it("contains a single error: a 'required' error for isInSale", () => {
-            filterErrors(tree)
+        _validate({isInSale: 5})
+        const invalid = toTree(_validate.erorrs)
 
+        it("1. a 'required' error for isInSale", () => {
+            filterErrors(missing)
             // tree.node only includes isInSale;
             // tree.node.isInSale only has one error;
             // the error keyword is 'required'
         })
-    })
 
-    describe("invalid isInSale, no fields", () => {
-        _validate({isInSale: 5})
-        const tree = toTree(_validate.errors)
-
-        it("contains a single error: a 'type' error for isInSale", () => {
-            filterErrors(tree)
+        it("2. a 'type' error for isInSale", () => {
+            filterErrors(invalid)
         })
     })
 
-    describe("missing isInSale, invalid field", () => {
+    describe("one invalid field and: 1. missing isInSale; 2. invalid isInSale. Contains two errors:", () => {
         _validate({name: 5})
-        const tree = toTree(_validate.errors)
+        const missing =  toTree(_validate.errors)
 
-        it("contains two errors: a 'required' error for isInSale and 'type' error for name", () => {
-            filterErrors(tree)
-        })
-    })
-
-    describe("invalid isInSale, invalid field", () => {
         _validate({isInSale: 5, name: 5})
-        const tree = toTree(_validate.errors)
+        const invalid =  toTree(_validate.errors)
 
-        it("contains two errors: a 'type' error for isInSale and 'type' error for name", () => {
-            filterErrors(tree)
+        it("1. a 'required' error for isInSale and 'type' error for name", () => {
+            filterErrors(missing)
+        })
+
+        it("2. a 'type' error for isInSale and 'type' error for name", () => {
+            filterErrors(invalid)
         })
     })
 
