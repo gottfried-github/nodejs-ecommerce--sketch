1f39b1476205db54b65f0a46fb374cecc7548554
spti <tobetodee@gmail.com>
2022-04-11 20:27:25 +0300

    test: abstract bson validation tests away from _validateBSON

---
+++ test/product-validate_BSON-validate-data.js
@@ -0,0 +1,47 @@
+import {assert} from 'chai'
+
+import {ObjectId} from 'mongodb'
+import {BSONTypeError} from "bson"
+
+// import {toTree} from 'ajv-errors-to-data-tree'
+
+function bsonValidateData(validate) {
+    describe("itemInitial is an invalid string objectId", () => {
+        const id = "an invalid id"
+        const fields = {itemInitial: id}
+        const errors = validate(fields)
+
+        it("returns truthy", () => {
+            assert(!!errors)
+        })
+
+        it("contains a single error, regarding itemInitial", () => {
+            assert(
+                // itemInitial is the only node and it has only one error
+                1 === Object.keys(errors.node).length && 'itemInitial' in errors.node && 1 === errors.node.itemInitial.errors.length
+            )
+        })
+
+        it("the error is the same as ObjectId's", () => {
+            try {
+                new ObjectId(id)
+            } catch(e) {
+                assert(
+                    // error is a BSONTypeError and is the same as the error thrown by ObjectId
+                    errors.node.itemInitial.errors[0] instanceof BSONTypeError
+                    && e.message === errors.node.itemInitial.errors[0].message
+                )
+            }
+        })
+    })
+
+    describe("itemInitial is a valid string objectId", () => {
+        const fields = {itemInitial: new ObjectId().toString()}
+
+        it("returns null", () => {
+            assert.strictEqual(validate(fields), null)
+        })
+    })
+}
+
+export {bsonValidateData}
