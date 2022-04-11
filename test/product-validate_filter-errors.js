8355909fd2fb4d3e116613080bd13789074220a6
spti <tobetodee@gmail.com>
2022-04-11 17:35:30 +0300

    test: abstract away assertions from the filterErrors: rather map them to the input data

--- test/product-validate_filter-errors.js
+++ test/product-validate_filter-errors.js
@@ -1,96 +1,61 @@
-import {assert} from 'chai'
-
 import {toTree} from 'ajv-errors-to-data-tree'
+
+import {jsonValidateData} from './product-validate_JSON-validate-data.js'
+
 import {filterErrors, _validate} from '../src/_product-validate.js'
 
 function testFilterErrors() {
     describe("no fields and: 1. missing isInSale; 2. invalid isInSale. Contains a single error:", () => {
-        _validate({})
+        _validate(jsonValidateData.missingNoFields.data)
         const missing = toTree(_validate.errors)
 
-        _validate({isInSale: 5})
+        _validate(jsonValidateData.invalidNoFields.data)
         const invalid = toTree(_validate.errors)
 
-        it("1. a 'required' error for isInSale", () => {
+        it(`1. ${jsonValidateData.missingNoFields.description}`, () => {
             filterErrors(missing)
 
             // console.log("testFilterErrors, no fields, missing, contains a 'required' error for isInSale, missing:", missing);
-            assert(
-                // tree.node only includes isInSale;
-                1 === Object.keys(missing.node).length && 'isInSale' in missing.node
-
-                // tree.node.isInSale only has one error;
-                // the error keyword is 'required'
-                && 1 === missing.node.isInSale.errors.length && 'required' === missing.node.isInSale.errors[0].data.keyword
-            )
+            jsonValidateData.missingNoFields.assert(missing)
         })
 
-        it("2. a 'type' error for isInSale", () => {
+        it(`2. ${jsonValidateData.invalidNoFields.description}`, () => {
             filterErrors(invalid)
 
             // console.log("testFilterErrors, no fields, missing, contains a 'required' error for isInSale, missing:", invalid.node.isInSale.errors);
-            assert(
-                // tree.node only includes isInSale;
-                1 === Object.keys(invalid.node).length && 'isInSale' in invalid.node
-
-                // tree.node.isInSale only has one error;
-                // the error keyword is 'type'
-                && 1 === invalid.node.isInSale.errors.length && 'type' === invalid.node.isInSale.errors[0].data.keyword
-            )
+            jsonValidateData.invalidNoFields.assert(invalid)
         })
     })
 
     describe("one invalid field and: 1. missing isInSale; 2. invalid isInSale. Contains two errors:", () => {
-        _validate({name: 5})
+        _validate(jsonValidateData.missingInvalidField.data)
         const missing =  toTree(_validate.errors)
 
-        _validate({isInSale: 5, name: 5})
+        _validate(jsonValidateData.invalidInvalidField.data)
         const invalid =  toTree(_validate.errors)
 
-        it("1. a 'required' error for isInSale and 'type' error for name", () => {
+        it(`1. ${jsonValidateData.missingInvalidField.description}`, () => {
             filterErrors(missing)
             // console.log("testFilterErrors, one invalid field, missing, filtered errors - missing:", missing.node);
 
-            const keys = Object.keys(missing.node)
-            assert(
-                // tree.node only includes the two fields
-                2 === keys.length && keys.includes('isInSale') && keys.includes('name')
-
-                // each of the fields have one proper error
-                && 1 === missing.node.isInSale.errors.length && 'required' === missing.node.isInSale.errors[0].data.keyword
-                && 1 === missing.node.name.errors.length && 'type' === missing.node.name.errors[0].data.keyword
-            )
+            jsonValidateData.missingInvalidField.assert(missing)
         })
 
-        it("2. a 'type' error for isInSale and 'type' error for name", () => {
+        it(`1. ${jsonValidateData.invalidInvalidField.description}`, () => {
             filterErrors(invalid)
 
-            const keys = Object.keys(invalid.node)
-            assert(
-                // tree.node only includes the two fields
-                2 === keys.length && keys.includes('isInSale') && keys.includes('name')
-
-                // each of the fields have one proper error
-                && 1 === invalid.node.isInSale.errors.length && 'type' === invalid.node.isInSale.errors[0].data.keyword
-                && 1 === invalid.node.name.errors.length && 'type' === invalid.node.name.errors[0].data.keyword
-            )
+            jsonValidateData.invalidInvalidField.assert(invalid)
         })
     })
 
     describe("true isInSale, invalid field", () => {
-        _validate({isInSale: true, name: 5})
+        _validate(jsonValidateData.trueInvalidField.data)
         const tree = toTree(_validate.errors)
 
-        it("contains two errors: a 'type' error for name and a 'required' error for itemInitial", () => {
+        it(jsonValidateData.trueInvalidField.description, () => {
             filterErrors(tree)
 
-            const keys = Object.keys(tree.node)
-            assert(
-                2 === keys.length && keys.includes('name') && keys.includes('itemInitial')
-
-                && 1 === tree.node.name.errors.length && 'type' === tree.node.name.errors[0].data.keyword
-                && 1 === tree.node.itemInitial.errors.length && 'required' === tree.node.itemInitial.errors[0].data.keyword
-            )
+            jsonValidateData.trueInvalidField.assert(tree)
         })
     })
 }
