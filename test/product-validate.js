9c8030213a480eab39cbaf1a2d814d47e5559e01
spti <tobetodee@gmail.com>
2022-04-12 19:38:02 +0300

    test, testJSONErrors: assert correct errors in testJSONErrors; define input data to test filterErrors and use testJSONErrors with it;

--- test/product-validate.js
+++ test/product-validate.js
@@ -2,13 +2,13 @@ import {testFilterErrors} from './product-validate_filter-errors.js'
 import {testValidateBSON} from './product-validate_validateBSON.js'
 // import {testValidate} from './product-validate_validate.js'
 
-// describe("filterErrors", () => {
-//     testFilterErrors()
-// })
-
-describe("_validateBSON", () => {
-    testValidateBSON()
+describe("filterErrors", () => {
+    testFilterErrors()
 })
+
+// describe("_validateBSON", () => {
+//     testValidateBSON()
+// })
 
 // describe("validate", () => {
 //     testValidate()
--- test/product-validate_filter-errors.js
+++ test/product-validate_filter-errors.js
@@ -1,63 +1,54 @@
 import {toTree} from 'ajv-errors-to-data-tree'
-
-import {jsonValidateData} from './product-validate_JSON-validate-data.js'
+import {testJSONErrors} from './product-validate_testJSONErrors.js'
 
 import {filterErrors, _validate} from '../src/_product-validate.js'
 
 function testFilterErrors() {
-    describe("no fields and: 1. missing isInSale; 2. invalid isInSale. Contains a single error:", () => {
-        _validate(jsonValidateData.missingNoFields.data)
-        const missing = toTree(_validate.errors)
+    const tests = {
+        isInSaleRequired: [{
+            i: [((data) => {
+                _validate(data)
+                // console.log("filterErrors tests, isInSaleRequired, i - data:", data);
+                return toTree(_validate.errors)
+            })({})],
+            o: (errors) => {filterErrors(errors); return errors},
+            description: "isInSale missing and no fields"
+        }],
+        isInSaleType: [{
+            i: [((data) => {
+                _validate(data)
+                return toTree(_validate.errors)
+            })({isInSale: 5})],
+            o: (errors) => {filterErrors(errors); return errors},
+            description: "isInSale invalid and no fields"
+        }],
+        isInSaleRequiredNameType: [{
+            i: [((data) => {
+                _validate(data)
+                return toTree(_validate.errors)
+            })({name: 5})],
+            o: (errors) => {filterErrors(errors); return errors},
+            description: "isInSale missing and name invalid"
+        }],
+        isInSaleNameType: [{
+            i: [((data) => {
+                _validate(data)
+                return toTree(_validate.errors)
+            })({isInSale: 5, name: 5})],
+            o: (errors) => {filterErrors(errors); return errors},
+            description: "isInSale invalid and name invalid"
+        }],
+        nameTypeItemInitialRequired: [{
+            i: [((data) => {
+                _validate(data)
+                return toTree(_validate.errors)
+            })({isInSale: true, name: 5})],
+            o: (errors) => {filterErrors(errors); return errors},
+            description: "isInSale true and name invalid"
+        }],
+    }
 
-        _validate(jsonValidateData.invalidNoFields.data)
-        const invalid = toTree(_validate.errors)
-
-        it(`1. ${jsonValidateData.missingNoFields.description}`, () => {
-            filterErrors(missing)
-
-            // console.log("testFilterErrors, no fields, missing, contains a 'required' error for isInSale, missing:", missing);
-            jsonValidateData.missingNoFields.assert(missing)
-        })
-
-        it(`2. ${jsonValidateData.invalidNoFields.description}`, () => {
-            filterErrors(invalid)
-
-            // console.log("testFilterErrors, no fields, missing, contains a 'required' error for isInSale, missing:", invalid.node.isInSale.errors);
-            jsonValidateData.invalidNoFields.assert(invalid)
-        })
-    })
-
-    describe("one invalid field and: 1. missing isInSale; 2. invalid isInSale. Contains two errors:", () => {
-        _validate(jsonValidateData.missingInvalidField.data)
-        const missing =  toTree(_validate.errors)
-
-        _validate(jsonValidateData.invalidInvalidField.data)
-        const invalid =  toTree(_validate.errors)
-
-        it(`1. ${jsonValidateData.missingInvalidField.description}`, () => {
-            filterErrors(missing)
-            // console.log("testFilterErrors, one invalid field, missing, filtered errors - missing:", missing.node);
-
-            jsonValidateData.missingInvalidField.assert(missing)
-        })
-
-        it(`1. ${jsonValidateData.invalidInvalidField.description}`, () => {
-            filterErrors(invalid)
-
-            jsonValidateData.invalidInvalidField.assert(invalid)
-        })
-    })
-
-    describe("true isInSale, invalid field", () => {
-        _validate(jsonValidateData.trueInvalidField.data)
-        const tree = toTree(_validate.errors)
-
-        it(jsonValidateData.trueInvalidField.description, () => {
-            filterErrors(tree)
-
-            jsonValidateData.trueInvalidField.assert(tree)
-        })
-    })
+    testJSONErrors(tests)
 }
 
 export {testFilterErrors}
